b4530a1ce7f77932a5d3f8e6d71c847fc27d49e9
Moritz Johner <beller.moritz@googlemail.com>
2020-03-02 15:00:33 +0100

    fix: e2e test naming

--- test/e2e/sync.go
+++ test/e2e/sync.go
@@ -8,7 +8,6 @@ import (
 	"github.com/moolen/harbor-sync/pkg/harbor"
 	"github.com/moolen/harbor-sync/test/e2e/framework"
 	"github.com/onsi/ginkgo"
-	log "github.com/sirupsen/logrus"
 )
 
 var _ = ginkgo.Describe("[Sync]", func() {
@@ -45,18 +44,19 @@ var _ = ginkgo.Describe("[Sync]", func() {
 	})
 
 	ginkgo.It("should do sync robot accounts", func() {
-		log.Infof("foo: %s", f.BaseName)
 		err := framework.WaitForSecret(
 			f.KubeClientSet,
 			framework.DefaultTimeout,
 			fmt.Sprintf("proj-%s-foo-pull-secret", f.Namespace),
-			fmt.Sprintf("team-%s-foo", f.Namespace), map[string]string{})
+			fmt.Sprintf("team-%s-foo", f.Namespace),
+			map[string]string{})
 		assert.Nil(ginkgo.GinkgoT(), err, "error syncing robot account")
 		err = framework.WaitForSecret(
 			f.KubeClientSet,
 			framework.DefaultTimeout,
 			fmt.Sprintf("proj-%s-bar-pull-secret", f.Namespace),
-			fmt.Sprintf("team-%s-bar", f.Namespace), map[string]string{})
+			fmt.Sprintf("team-%s-bar", f.Namespace),
+			map[string]string{})
 		assert.Nil(ginkgo.GinkgoT(), err, "error syncing robot account")
 	})
 })
